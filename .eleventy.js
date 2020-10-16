93c62c624bf0efdd93b0eb4f7ef74e89b448cc61
Simon Willans <stwillans@gmail.com>
2020-10-16 11:04:30 +0100

    Adds base layout

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,9 @@ module.exports =  {
 	dir: {
 		input: 'resources/views',
 		output: 'public',
-		includes: 'resources/views/_includes',
-		layouts: 'resources/views/_layouts',
-		data: 'resources/views/_data',
+		includes: '_includes',
+		layouts: '_layouts',
+		data: '_data',
 	},
 	env: process.env.NODE_ENV,
 };
