710ab0ca571263bc5785e29c420a8ff7e34ab024
Simon Willans <simon@engageinteractive.co.uk>
2021-08-28 10:57:04 +0100

    Adds eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const Image = require('@11ty/eleventy-img');
+const reverse = require('lodash/reverse');
+
 const options = {
 	dir: {
 		input: 'src/app',
@@ -11,6 +14,54 @@ const options = {
 	// htmlTemplateEngine: 'liquid',
 	// dataTemplateEngine: 'liquid',
 	// markdownTemplateEngine: 'liquid',
+};
+
+const imageConfig = {
+	transforms: {
+		default: [
+			{
+				min: 1248,
+				width: 400,
+			},
+			{
+				min: 768,
+				width: 200,
+			},
+			{
+				width: 100,
+			},
+		],
+	},
+};
+
+const imgShortcode = async (src, alt, config = 'default', outputFormat = ['webp','jpeg']) => {
+	if(alt === undefined) {
+		throw new Error(`Missing \`alt\` on myResponsiveImage from: ${src}`);
+	}
+
+	const transform = imageConfig.transforms[config];
+	const widths = Object.values(transform.map(x => x.width));
+
+	let stats = await Image(src, {
+		urlPath: '/compiled/img',
+		outputDir: `./${options.dir.output}/compiled/img`,
+		widths: widths,
+		formats: outputFormat,
+		// dryRun: true,
+	});
+
+	console.log('transform', transform);
+	console.log('widths', widths);
+	console.log('stats', stats);
+
+	const img = {
+		src: stats.jpeg ? reverse(Object.values(stats.jpeg.map(x => x.url))) : null,
+		webp: stats.webp ? reverse(Object.values(stats.webp.map(x => x.url))) : null,
+		sizes: Object.values(transform.filter(x => x.min).map(x => x.min)),
+		alt,
+	};
+
+	return JSON.stringify(img);
 };
 
 module.exports = (eleventyConfig) => {
@@ -27,6 +78,8 @@ module.exports = (eleventyConfig) => {
 		}
 	});
 
+	eleventyConfig.addLiquidShortcode('img', imgShortcode);
+
 	eleventyConfig.setBrowserSyncConfig({
 		files: [
 			'_site/compiled/css',
@@ -36,6 +89,10 @@ module.exports = (eleventyConfig) => {
 		],
 		port: 3001,
 	});
+
+	// testing
+	// const newimage = imgShortcode('https://images.unsplash.com/photo-1608178398319-48f814d0750c', 'ALT TEXT HERE');
+	// const newimage = imgShortcode('https://res.cloudinary.com/demo/image/upload/w_400,c_crop,ar_1:2,g_face/lady.jpg', 'ALT TEXT HERE');
 
 	return options;
 };
